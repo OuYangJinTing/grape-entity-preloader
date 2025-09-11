b2b02d624a1afbd85be43b94751aaf6c0e9553de
OuYangJinTing <Ou.Yang.Jin.Ting.X@gmail.com>
2025-09-11 17:39:36 +0800

    Init repo through bundle gem command

---
+++ lib/grape/entity/preloader.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "preloader/version"
+
+module Grape
+  module Entity
+    module Preloader
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/grape/entity/preloader/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Grape
+  module Entity
+    module Preloader
+      VERSION = "0.1.0"
+    end
+  end
+end
