66cd8a4a35cff7dd942ea41eee7d13c0b9bfcb88
OuYangJinTing <Ou.Yang.Jin.Ting.X@gmail.com>
2025-09-25 17:42:11 +0800

    Change rspec test framework

---
+++ spec/grape/entity/preloader_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Grape::Entity::Preloader do
+  it 'has a version number' do
+    expect(Grape::Entity::Preloader::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
