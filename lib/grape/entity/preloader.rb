0ea2e294e2c8b443340ba585a26ff9b0762b95c3
OuYangJinTing <Ou.Yang.Jin.Ting.X@gmail.com>
2025-10-08 18:55:56 +0800

    Fix Preloader with multi preload_callback maybe invalid

--- lib/grape/entity/preloader.rb
+++ lib/grape/entity/preloader.rb
@@ -9,78 +9,88 @@ require_relative 'preloader/exposure/base'
 module Grape
   class Entity
     class Preloader # rubocop:disable Style/Documentation
-      attr_reader :exposures, :objects, :options
+      attr_reader :exposures, :objects, :options, :associations, :callbacks, :nested_association_chain
+
+      def self.activerecord_gte_7_0?
+        unless defined?(ActiveRecord) && ActiveRecord.respond_to?(:version) && ActiveRecord.version >= Gem::Version.new('7.0')
+          warn 'ActiveRecord 7.0 or later is required for preload association'
+          return false
+        end
+
+        true
+      end
 
       def initialize(exposures, objects, options)
         @exposures = exposures
         @objects = Array.wrap(objects)
         @options = options
+
+        @associations = {}
+        @callbacks = Hash.new { |hash, key| hash[key] = [] }
+        @nested_association_chain = []
       end
 
-      if defined?(ActiveRecord) && ActiveRecord.respond_to?(:version) && ActiveRecord.version >= Gem::Version.new('7.0')
-        def call
-          associations = {}
-          exposures_with_callback = {}
-          extract_preload_options(exposures, options, associations, exposures_with_callback)
-          preload_associations(objects, associations)
-          preload_callbacks(objects, exposures_with_callback)
-        end
-      else
-        def call
-          warn 'The Preloader work normally requires activerecord(>= 7.0) gem'
-        end
+      def call
+        return if objects.empty?
+
+        extract_preload_options(exposures, options, associations)
+        execute_preload_associations
+        execute_preload_callbacks
       end
 
       private
 
-      def preload_associations(objects, associations)
+      def execute_preload_associations
+        return unless Preloader.activerecord_gte_7_0?
+
         # TODO: Change ActiveRecord async query
         ActiveRecord::Associations::Preloader.new(records: objects, associations: associations).call
       end
 
-      def preload_callbacks(objects, exposures_with_callback)
-        exposures_with_callback.each do |association_paths, (exposure, options)|
-          association_objects = objects
-          association_paths.split('.').each do |association_path|
-            association_objects = association_objects.map { |object| object.send(association_path) }.flatten(1)
+      def execute_preload_callbacks # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+        callbacks.each do |association_chain, exposures_with_options|
+          association_objects = association_chain.inject(objects) do |items, association|
+            items.filter_map(&association).flatten(1)
           end
-          callback_objects = exposure.preload_callback.call(association_objects)
+          next if association_objects.empty?
 
-          if exposure.is_a?(Grape::Entity::Exposure::RepresentExposure)
-            Preloader.new(exposure.using_class.root_exposures, callback_objects, options).call
+          exposures_with_options.each do |exposure, options|
+            callback_objects = exposure.preload_callback.call(association_objects)
+            if exposure.is_a?(Grape::Entity::Exposure::RepresentExposure)
+              Preloader.new(exposure.using_class.root_exposures, callback_objects, options).call
+            end
           end
         end
       end
 
-      def extract_preload_options(exposures, options, associations, exposures_with_callback)
+      def extract_preload_options(exposures, options, associations) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity,Metrics/MethodLength
         exposures.each do |exposure|
           next unless exposure.should_return_key?(options)
-          next exposures_with_callback[associations.keys.join('.')] = [exposure, options] if exposure.preload_callback
 
-          associations[exposure.preload_association] ||= {} if exposure.preload_association
-          deep_extract_preload_options(exposure, options, associations, exposures_with_callback)
-        end
-      end
+          if exposure.preload_callback
+            callbacks[nested_association_chain.dup] << [exposure, options]
+          elsif exposure.preload_association && Preloader.activerecord_gte_7_0?
+            associations[exposure.preload_association] ||= {}
+          end
 
-      def deep_extract_preload_options(exposure, options, associations, exposures_with_callback) # rubocop:disable Metrics/MethodLength
-        key_of_exposure = exposure.instance_variable_get(:@key)
-        # Dynamic keys are difficult to handle and less used, skipped directly
-        return if key_of_exposure.respond_to?(:call)
+          key_of_exposure = exposure.instance_variable_get(:@key)
+          # Dynamic keys are difficult to handle and less used, skipped directly
+          next if key_of_exposure.respond_to?(:call)
 
-        if exposure.is_a?(Grape::Entity::Exposure::NestingExposure)
-          extract_preload_options(
-            exposure.nested_exposures,
-            options.for_nesting(key_of_exposure),
-            associations,
-            exposures_with_callback
-          )
-        elsif exposure.is_a?(Grape::Entity::Exposure::RepresentExposure) && exposure.preload_association
-          extract_preload_options(
-            exposure.using_class.root_exposures,
-            options.for_nesting(key_of_exposure),
-            associations[exposure.preload_association],
-            exposures_with_callback
-          )
+          if exposure.is_a?(Grape::Entity::Exposure::NestingExposure)
+            extract_preload_options(
+              exposure.nested_exposures,
+              options.for_nesting(key_of_exposure),
+              associations
+            )
+          elsif exposure.is_a?(Grape::Entity::Exposure::RepresentExposure) && associations[exposure.preload_association]
+            nested_association_chain << exposure.preload_association
+            extract_preload_options(
+              exposure.using_class.root_exposures,
+              options.for_nesting(key_of_exposure),
+              associations[exposure.preload_association]
+            )
+          end
         end
       end
     end
